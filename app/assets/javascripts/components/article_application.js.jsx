40f26cd148a2609daaea91b115b09ada6cc0c2a9
Aboli Dole <aboli.dole@gmail.com>
2017-10-08 08:39:22 -0400

    Initial commit-created first component

---
+++ app/assets/javascripts/components/article_application.js.jsx
@@ -0,0 +1,12 @@
+var ArticleApplication = React.createClass({
+  render: function() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>React_Rails CC</h1>
+          <p>by:Aboli Dole</p>
+        </div>
+      </div>
+        )
+      }
+    });
